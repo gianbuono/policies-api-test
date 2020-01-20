653f0f325e64ac4b14d73e0693a64dcbea1c5b48
Giancarlo Buono <gcarlo.buono@gmail.com>
2020-01-20 13:22:53 +0100

    + mongoose + cors + dotenv

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
 
 app.use(logger('dev'));
-api.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
